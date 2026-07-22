b04234a6f31bb0925e9734b1c139ee50e3b9de88
mpancino-png <mpancino@gmail.com>
2026-07-22 12:32:16 +0200

    Add Spring Boot main application class

---
+++ src/main/java/com/kinantobio/GuestGuideApplication.java
@@ -0,0 +1,13 @@
+package com.kinantobio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuestGuideApplication.class, args);
+    }
+
+}
